f70e18e30659b719717afc08922947c3333f6585
blackhathckr <neerajvn1@gmail.com>
2025-09-13 13:03:08 +0530

    dist

--- dist/registerSW.js
+++ dist/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/ARdrobe/sw.js', { scope: '/ARdrobe/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
